279db3e236e0796468e4fc244936174bde4929c4
Sunnice <44739306+Sunnice@users.noreply.github.com>
2020-04-13 00:21:47 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -18,6 +18,7 @@ git clone https://github.com/skyformat99/eqos.git package/eqos
 git clone https://github.com/ElonH/Rclone-OpenWrt.git package/rclone
 git clone https://github.com/Sunnice/luci-app-vssr.git package/vssr
 git clone https://github.com/frainzy1477/luci-app-clash.git package/clash
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/openwrt/luci.git package/openwrt
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/Lienol/luci-theme-darkmatter.git package/luci-theme-darkmatter
